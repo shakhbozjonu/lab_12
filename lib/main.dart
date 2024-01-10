caaa806c5027e917af3d2a537a8fe2c6a489fc49
Shakhbozjon Usmonov <115206579+shakhbozjonu@users.noreply.github.com>
2024-01-10 23:29:47 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-// Based on https://dartpad.dev/?id=d57c6c898dabb8c6fb41018588b8cf73
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -18,17 +17,10 @@ void main() async {
     print(st);
   }
 
-  // The first step to using Firebase is to configure it so that our code can
-  // find the Firebase project on the servers. This is not a security risk, as
-  // explained here: https://stackoverflow.com/a/37484053
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  // We sign the user in anonymously, meaning they get a user ID without having
-  // to provide credentials. While this doesn't allow us to identify the user,
-  // this would, for example, still allow us to associate data in the database
-  // with each user.
   await FirebaseAuth.instance.signInAnonymously();
 
   runApp(MyApp());
@@ -48,17 +40,6 @@ class MyApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // The user can send a message to Firebase. What they can send is
-              // protected by server-side security rules, which in this case
-              // only allow chat messages that this regular expression:
-              //
-              //    ^((?i)hello|\\s|firebase|welcome|to|summit|the|this|
-              //    everyone|good|morning|afternoon|firestore|meetup|
-              //    devfest|virtual|online)+
-              //
-              // In a real project you'd probably expand that, for example by
-              // only allowing users that you explicitly approve to post
-              // messages.
               const SizedBox(height: 32),
               Text(
                 'Enter a new message',
@@ -89,10 +70,7 @@ class MyApp extends StatelessWidget {
                   },
                 ),
               ),
-              // We use a stream builder to both read the initial data from the
-              // database and listen to updates to that data in realtime. The
-              // database we use is called Firestore, and we are asking the 10
-              // most recent messages.
+              
               const SizedBox(height: 32),
               Text(
                 'The latest messages',
